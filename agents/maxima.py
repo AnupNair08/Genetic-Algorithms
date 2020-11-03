aad897a897dca33e97eb70f3beed376c956f68fd
AnupNair08 <nair.anup09@gmail.com>
2020-11-03 09:56:47 +0530

    chore: final changes

--- agents/maxima.py
+++ agents/maxima.py
@@ -1,20 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-populationSize = int(input('Enter population size:\n'))       
-genomeSize = 10            
-rangeVal = [0, 10]         
+populationSize = int(input('Enter population size:\n'))
+genomeSize = 10
+rangeVal = [0, 10]
 
-#Hyperparameters to decide the rate of crossover and mutation
-CROSS_RATE = 0.7         
-MUTATION_RATE = 0.004    
+# Hyperparameters to decide the rate of crossover and mutation
+CROSS_RATE = 0.7
+MUTATION_RATE = 0.004
 
-def generateFunc(x): 
-    return np.sin(5*x)*x + np.cos(2*x)*x     
 
-#Fitness heuristic here is the differene in values between the minimum value and other values
-#Maximum fitness score is returned 
-def fitness(genome): 
+def generateFunc(x):
+    return np.sin(5*x)*x + np.cos(2*x)*x
+
+# Fitness heuristic here is the differene in values between the minimum value and other values
+# Maximum fitness score is returned
+
+
+def fitness(genome):
     return genome + 1e-3 - np.min(genome)
 
 
@@ -23,16 +26,17 @@ def normDNA(pop):
     return pop.dot(2 ** np.arange(genomeSize)[::-1]) / float(2**genomeSize-1) * rangeVal[1]
 
 
-def select(pop, fitness):    
-    ind = np.random.choice(np.arange(populationSize), size=populationSize, replace=True,p=fitness/fitness.sum())
+def select(pop, fitness):
+    ind = np.random.choice(np.arange(
+        populationSize), size=populationSize, replace=True, p=fitness/fitness.sum())
     return pop[ind]
 
 
-def crossover(parent, pop):     
+def crossover(parent, pop):
     if np.random.rand() < CROSS_RATE:
-        i_ = np.random.randint(0, populationSize, size=1)                             
-        cross_points = np.random.randint(0, 2, size=genomeSize).astype(np.bool)   
-        parent[cross_points] = pop[i_, cross_points]                           
+        i_ = np.random.randint(0, populationSize, size=1)
+        cross_points = np.random.randint(0, 2, size=genomeSize).astype(np.bool)
+        parent[cross_points] = pop[i_, cross_points]
     return parent
 
 
@@ -42,34 +46,37 @@ def mutate(child):
             child[point] = 1 if child[point] == 0 else 0
     return child
 
+
 if __name__ == "__main__":
-    #Plotting the initial function that is generated and normalised
-    plt.ion()       
+    print('Function maxima simulation')
+    # Plotting the initial function that is generated and normalised
+    plt.ion()
     x = np.linspace(*rangeVal, 200)
     plt.plot(x, generateFunc(x))
 
-    #Create the initial population
-    pop = np.random.randint(2, size=(populationSize, genomeSize)) 
+    # Create the initial population
+    pop = np.random.randint(2, size=(populationSize, genomeSize))
 
-
-    #Iterate for a set of generations
+    # Iterate for a set of generations
     for i in range(150):
-        F_values = generateFunc(normDNA(pop))  
+        F_values = generateFunc(normDNA(pop))
 
-        #Get all the values for the functions and scatter plot them
-        if 'sca' in globals(): 
+        # Get all the values for the functions and scatter plot them
+        if 'sca' in globals():
             sca.remove()
-        sca = plt.scatter(normDNA(pop), F_values, s=200, lw=0, c='green', alpha=0.5); plt.pause(0.1)
+        sca = plt.scatter(normDNA(pop), F_values, s=200,
+                          lw=0, c='green', alpha=0.5)
+        plt.pause(0.1)
 
-        #Calculate fitness and perform selection, crossover and mutation.
+        # Calculate fitness and perform selection, crossover and mutation.
         fitnessVal = fitness(F_values)
-        print("Generation {}: {}".format(i,pop[np.argmax(fitnessVal), :]))
+        print("Generation {}: {}".format(i, pop[np.argmax(fitnessVal), :]))
         pop = select(pop, fitnessVal)
         pop_copy = pop.copy()
         for parent in pop:
             child = crossover(parent, pop_copy)
             child = mutate(child)
-            parent[:] = child       
+            parent[:] = child
 
     plt.ioff()
     plt.show()
--- agents/wordmatcher.py
+++ agents/wordmatcher.py
@@ -1,30 +1,36 @@
 import random
 target = input('Enter target string:\n')
 
-#All possible generation size
+# All possible generation size
 gene = '''abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ 1234567890, .-;:_!"#%&/()=?@${[]}'''
 populationSize = int(input('Enter population size:\n'))
 
-#Function to calculate the fitness score of each individual
+# Function to calculate the fitness score of each individual
+
+
 def fitness(genome):
     score = 0
     for i in range(len(genome)):
-        if(genome[i] !=  target[i]):
+        if(genome[i] != target[i]):
             score += 1
     return score
 
-#Function to randomly mutate the gene of a individual
+# Function to randomly mutate the gene of a individual
+
+
 def mutate():
-    global gene 
+    global gene
     genome = ''
-    for i in range(len(target)):
+    for _ in range(len(target)):
         genome += (random.choice(gene))
     return genome
 
-#Function that produces an offspring from two differnt parents
-def crossover(p1,p2):
+# Function that produces an offspring from two differnt parents
+
+
+def crossover(p1, p2):
     child = ''
-    for i in range(len(p1)):
+    for _ in range(len(p1)):
         p = random.random()
         if(p < 0.5):
             child += p1[i]
@@ -34,21 +40,24 @@ def crossover(p1,p2):
             child += random.choice(gene)
     return child
 
+
 if __name__ == "__main__":
-    #A tunable hyper parameter in [0,1] that can be varied to select max number of fittest individuals without crossovers
+    print('Word Matcher simulation')
+
+    # A tunable hyper parameter in [0,1] that can be varied to select max number of fittest individuals without crossovers
     hyperParam = 0.2
-    
+
     generation = 0
     finish = 0
     population = []
-    #generate the initial population
+    # generate the initial population
     for i in range(populationSize):
         population.append(mutate())
-    
+
     while not finish:
-        #Lower the fitness score fitter the individual
-        population = sorted(population, key = lambda x: fitness(x))
-        #Match found
+        # Lower the fitness score fitter the individual
+        population = sorted(population, key=lambda x: fitness(x))
+        # Match found
         if(fitness(population[0]) == 0):
             finish = 1
             print(generation, population[0], fitness(population[0]))
@@ -57,12 +66,12 @@ if __name__ == "__main__":
         newgen = []
         for i in range(int(hyperParam * populationSize)):
             newgen.append(population[i])
-        for i in range(int((1- hyperParam) * populationSize)):
+        for i in range(int((1 - hyperParam) * populationSize)):
             p1 = random.choice(population[:50])
             p2 = random.choice(population[:50])
-            newgen.append(crossover(p1,p2))
+            newgen.append(crossover(p1, p2))
         population = newgen
         print(generation, population[0], fitness(population[0]))
         generation += 1
-    
+
     print("Computed matching string in {} generations" .format(generation))
